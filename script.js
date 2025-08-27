ee2375e71401af6ca6cbc7ca4b3dc7c0f13e6a78
Makostya19 <kma293385@gmail.com>
2025-08-27 12:18:19 +0900

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const mainBtnImg = mainBtn.querySelector("img");
 mainBtn.addEventListener("click", () => {
   if (extraBtns.style.display === "none" || extraBtns.style.display === "") {
     extraBtns.style.display = "flex"; // показываем кнопки
-    mainBtn.innerHTML = '<img src="./assets/icon.svg" alt="иконка"> Скрыть';
+    mainBtn.innerHTML = '<img src="./assets/icon2.svg" alt="иконка"> Скрыть';
   } else {
     extraBtns.style.display = "none"; // скрываем кнопки
     mainBtn.innerHTML = '<img src="./assets/icon.svg" alt="иконка"> Показать все';
